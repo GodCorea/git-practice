86bb426856abf96bbe1d3d3a6f2f25039d4058c5
nity-su <7anlyn@gmail.com>
2022-11-03 17:00:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <div style={{ height: "100vw" }}>
-      <Spline scene="https://prod.spline.design/6MvzYDm9vAIKrX09/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/VcPl1ZzdV1E6HfNE/scene.splinecode" />
     </div>
   );
 }
