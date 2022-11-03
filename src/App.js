2a1e0aa249c60f1661897e195989cc33932d44ab
GodCorea <4628c@naver.com>
2022-11-03 22:59:45 +0900

    good

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <div style={{ height: "100vw" }}>
-      <Spline scene="https://prod.spline.design/VcPl1ZzdV1E6HfNE/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/oE30ZgkikBNSbClB/scene.splinecode" />
     </div>
   );
 }
